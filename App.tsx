5aff2f794d161a20d4a45c32f0fe36fad6fb8b5e
smalake <41889150+smalake@users.noreply.github.com>
2024-01-18 19:26:07 +0900

    モーダル遷移できるように

--- App.tsx
+++ App.tsx
@@ -1,29 +1,59 @@
 import React from 'react';
 import 'react-native-gesture-handler';
-import { NavigationContainer } from '@react-navigation/native';
+import { NavigationContainer, useNavigation } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { Login } from './src/components/Login';
 import Home from './src/components/Home';
 import { Register } from './src/components/Register';
 import { VerifyEmail } from './src/components/VerifyEmail';
+import Event from './src/components/Event';
+import { Text, TouchableOpacity } from 'react-native';
 
 export type RootStackParamList = {
   Login: undefined;
   Register: undefined;
   VerifyEmail: undefined;
   Home: undefined;
+  Event: undefined;
 };
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
 
 const App = () => {
+  // ヘッダーボタンコンポーネントを作成します
+  const BackButton = () => {
+    const navigation = useNavigation();
+    return (
+      <TouchableOpacity
+        onPress={() => {
+          navigation.goBack();
+        }}
+        style={{ marginLeft: 10 }}
+      >
+        <Text style={{ color: 'blue' }}>閉じる</Text>
+      </TouchableOpacity>
+    );
+  };
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName='Login'>
-        <Stack.Screen name='Login' component={Login} options={{ headerShown: false, gestureEnabled: false }} />
+      <Stack.Navigator initialRouteName='Home'>
+        <Stack.Screen
+          name='Login'
+          component={Login}
+          options={{ headerShown: false, gestureEnabled: false, presentation: 'modal', animation: 'default' }}
+        />
         <Stack.Screen name='Register' component={Register} options={{ headerTitle: 'ユーザー登録' }} />
         <Stack.Screen name='VerifyEmail' component={VerifyEmail} options={{ headerShown: false, gestureEnabled: false }} />
         <Stack.Screen name='Home' component={Home} />
+        <Stack.Screen
+          name='Event'
+          component={Event}
+          options={{
+            presentation: 'modal',
+            animation: 'default',
+            headerLeft: () => <BackButton />,
+          }}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
