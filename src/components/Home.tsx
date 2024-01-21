d90e64fcf0ad53ec56e0ea6ad59e927fbbf2e648
smalake <41889150+smalake@users.noreply.github.com>
2024-01-21 21:55:37 +0900

    イベント一覧を表示する機能を実装

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,110 +1,71 @@
 import React, { useState } from 'react';
-import { SafeAreaView, Text, View, StyleSheet, TouchableHighlight, FlatList } from 'react-native';
+import { SafeAreaView, Text, View, StyleSheet, TouchableHighlight, FlatList, Image } from 'react-native';
 import { LoginCheck } from './LoginCheck';
-import { useForm, Controller } from 'react-hook-form';
-import { yupResolver } from '@hookform/resolvers/yup';
-import { homeValidation } from '../util/Validation';
-import { Button, Dialog, Icon, Input, Overlay } from 'react-native-elements';
-import DateTimePickerModal from 'react-native-modal-datetime-picker';
+import { Icon } from 'react-native-elements';
 import { memoApi } from '../api/memo';
 import { useNavigation } from '@react-navigation/native';
 import { NativeStackNavigationProp } from '@react-navigation/native-stack';
 import { RootStackParamList } from '../../App';
 
-type FormData = {
-  amount: string;
-  partner: string;
-  date?: Date;
-  period?: Date;
-  memo?: string;
-};
 const Home: React.FC = () => {
   const navigation = useNavigation<NativeStackNavigationProp<RootStackParamList>>();
-  const [visible, setVisible] = useState(false);
   const [loading, setLoading] = useState(false); // ログインボタンのローディング表示
-  const [date, setDate] = useState(new Date());
-  const [dateVasible, setDateVisible] = useState(false);
-  const [period, setPeriod] = useState(new Date());
-  const [periodVisible, setPeriodVisible] = useState(false);
   const testData = [
-    { id: 1, amount: 1000, partner: 'test', date: '2021-10-01', period: '2021-10-31', memo: 'test' },
-    { id: 2, amount: 2000, partner: 'test2', date: '2021-10-01', period: '2021-10-31', memo: 'test2' },
-    { id: 3, amount: 3000, partner: 'test3', date: '2021-10-01', period: '2021-10-31', memo: 'test3' },
-    { id: 4, amount: 4000, partner: 'test4', date: '2021-10-01', period: '2021-10-31', memo: 'test4' },
+    { id: 1, amount: 1000, partner: 'test', date: '2021-10-01', period: '2021-10-31', memo: 'test', type: 0 },
+    { id: 2, amount: 2000, partner: 'test2', date: '2021-10-01', period: '2021-10-31', memo: 'test2', type: 1 },
+    { id: 3, amount: 3000, partner: 'test3', date: '2021-10-01', period: '2021-10-31', memo: 'test3', type: 0 },
+    { id: 4, amount: 4000, partner: 'test4', date: '2021-10-01', period: '2021-10-31', memo: 'test4', type: 1 },
   ];
-  const testHeader = ['取引相手', '金額', '取引日', '期限日', 'メモ'];
-  const {
-    handleSubmit,
-    control,
-    formState: { errors },
-  } = useForm<FormData>({ resolver: yupResolver(homeValidation) });
 
-  const toggleDate = () => {
-    setDateVisible(!dateVasible);
-  };
-  const togglePeriod = () => {
-    setPeriodVisible(!periodVisible);
-  };
-
-  const handleConfirm = (date: Date) => {
-    setDate(date);
-    toggleDate();
-  };
-  const handlePeriodConfirm = (date: Date) => {
-    setPeriod(date);
-    togglePeriod();
-  };
+  const typeValue = ['貸し', '借り'];
 
   const toggleDialog = () => {
-    setVisible(!visible);
+    // setVisible(!visible);
+    navigation.navigate('Event');
   };
   const onPushed = (id: number) => {
     console.log(id);
   };
-  const onSubmit = async (data: FormData) => {
-    setLoading(true);
-    try {
-      const req = { ...data, amount: Number(data.amount), date: date, period: period };
-      const res = await memoApi.register(req);
-      if (res.status === 200) {
-        alert('登録しました');
-      } else if (res.status === 401) {
-        alert('再ログインしてください');
-        navigation.navigate('Login');
-      } else {
-        alert('登録に失敗しました');
-      }
-    } catch (error) {
-      alert('登録に失敗しました');
-      console.log(error);
-    } finally {
-      setLoading(false);
-    }
-  };
   return (
     <SafeAreaView style={styles.container}>
-      <View style={styles.listStyle}>
-        <Text style={styles.listItemPartner}>取引相手</Text>
-        <Text style={styles.listItemAmount}>金額</Text>
-        <Text style={styles.listItemDate}>取引日</Text>
-        <Text style={styles.listItemPeriod}>期限日</Text>
-        <Text style={styles.listItemMemo}>メモ</Text>
-      </View>
       <FlatList
         data={testData}
+        style={{ flex: 1 }}
         renderItem={({ item }) => (
           <TouchableHighlight
+            style={{ flex: 1 }}
             onPress={() => {
               onPushed(item.id);
             }}
             underlayColor={'blue'}
           >
             <View style={styles.listStyle}>
-              <Text style={styles.listItemPartner}>{item.partner}</Text>
-              <Text style={styles.listItemAmount}>{item.amount}</Text>
-              <Text style={styles.listItemDate}>{item.date}</Text>
-              <Text style={styles.listItemPeriod}>{item.period}</Text>
-              <Text style={styles.listItemMemo}>{item.memo}</Text>
+              <View style={styles.listItemDate}>
+                <Text style={{ fontWeight: 'bold' }}>
+                  {item.date} 〜 {item.period}
+                </Text>
+              </View>
+              <View style={{ justifyContent: 'space-between', flexDirection: 'row' }}>
+                <View style={{ flexDirection: 'row', alignSelf: 'center', alignItems: 'center' }}>
+                  <View>
+                    {item.type === 0 ? (
+                      <Image source={require('../../assets/kari.png')} style={styles.icon} />
+                    ) : (
+                      <Image source={require('../../assets/kasi.png')} style={styles.icon} />
+                    )}
+                  </View>
+                  <View>
+                    <Text style={styles.listItemAmount}>{item.amount}</Text>
+                  </View>
+                </View>
+                <View style={{ alignSelf: 'flex-end' }}>
+                  <Text>
+                    {item.partner}に{typeValue[item.type]}
+                  </Text>
+                  <Text style={{ textAlign: 'right' }}>{item.memo}</Text>
+                </View>
+              </View>
+              <View style={{ height: 1, backgroundColor: 'gray', marginTop: 10 }} />
             </View>
           </TouchableHighlight>
         )}
@@ -114,143 +75,6 @@ const Home: React.FC = () => {
       <View style={styles.addButton}>
         <Icon name='plus-circle' size={50} color={'skyblue'} type='font-awesome-5' onPress={toggleDialog} />
       </View>
-      <Overlay isVisible={visible} overlayStyle={{ paddingTop: 100 }} fullScreen={true}>
-        <View style={styles.formLabel}>
-          <Text>金額</Text>
-        </View>
-        <View>
-          <Controller
-            control={control}
-            name='amount'
-            rules={{ required: true }}
-            render={({ field: { onChange, onBlur, value } }) => (
-              <Input
-                placeholder='金額を入力'
-                keyboardType='number-pad'
-                onBlur={onBlur}
-                value={value}
-                onChangeText={(value) => {
-                  // 数字以外の入力を無効化
-                  const numericValue = value.replace(/[^0-9]/g, '');
-                  onChange(numericValue);
-                }}
-              />
-            )}
-          />
-        </View>
-        <View style={styles.error}>{errors.amount && <Text style={{ color: 'red', textAlign: 'left' }}>{errors.amount.message}</Text>}</View>
-        <View style={styles.formLabel}>
-          <Text>取引相手</Text>
-        </View>
-        <View>
-          <Controller
-            control={control}
-            name='partner'
-            rules={{ required: true }}
-            render={({ field: { onChange, onBlur, value } }) => (
-              <Input
-                placeholder='取引相手を入力'
-                onBlur={onBlur}
-                value={value}
-                onChangeText={(value) => {
-                  onChange(value);
-                }}
-              />
-            )}
-          />
-        </View>
-        <View style={styles.error}>{errors.partner && <Text style={{ color: 'red', textAlign: 'left' }}>{errors.partner.message}</Text>}</View>
-        <View style={styles.formLabel}>
-          <Text>メモ</Text>
-        </View>
-        <View>
-          <Controller
-            control={control}
-            name='memo'
-            // rules={{ required: true }}
-            render={({ field: { onChange, onBlur, value } }) => (
-              <Input
-                placeholder='メモを入力'
-                onBlur={onBlur}
-                value={value}
-                onChangeText={(value) => {
-                  onChange(value);
-                }}
-              />
-            )}
-          />
-        </View>
-        <View style={styles.formLabel}>
-          <Text>登録日</Text>
-        </View>
-        <View style={styles.date}>
-          <View style={styles.dateInput}>
-            <Controller
-              control={control}
-              name='date'
-              rules={{ required: true }}
-              render={({ field: { onChange, onBlur, value } }) => (
-                <Input
-                  disabled
-                  style={{ color: 'black' }}
-                  onBlur={onBlur}
-                  value={date.toLocaleDateString()}
-                  onChangeText={(value) => {
-                    onChange(value);
-                  }}
-                />
-              )}
-            />
-          </View>
-          <View style={styles.dateButton}>
-            <Icon name='calendar' type='font-awesome-5' onPress={toggleDate} />
-            <DateTimePickerModal isVisible={dateVasible} locale='ja-JP' mode='date' onConfirm={handleConfirm} onCancel={toggleDate} />
-          </View>
-        </View>
-        <View style={styles.formLabel}>
-          <Text>期限日</Text>
-        </View>
-        <View style={styles.date}>
-          <View style={styles.dateInput}>
-            <Controller
-              control={control}
-              name='period'
-              rules={{ required: true }}
-              render={({ field: { onChange, onBlur, value } }) => (
-                <Input
-                  disabled
-                  style={{ color: 'black' }}
-                  onBlur={onBlur}
-                  value={period.toLocaleDateString()}
-                  onChangeText={(value) => {
-                    onChange(value);
-                  }}
-                />
-              )}
-            />
-          </View>
-          <View style={styles.dateButton}>
-            <Icon name='calendar' type='font-awesome-5' onPress={togglePeriod} />
-            <DateTimePickerModal isVisible={periodVisible} locale='ja-JP' mode='date' onConfirm={handlePeriodConfirm} onCancel={togglePeriod} />
-          </View>
-        </View>
-        <View style={styles.buttonArea}>
-          <View style={styles.button}>
-            <Button title='登録' titleStyle={{ fontWeight: 'bold' }} loading={loading} onPress={handleSubmit(onSubmit)} />
-          </View>
-          <View style={styles.button}>
-            <Button
-              title='キャンセル'
-              buttonStyle={{
-                backgroundColor: 'gainsboro',
-                borderRadius: 3,
-              }}
-              titleStyle={{ color: 'black' }}
-              onPress={toggleDialog}
-            />
-          </View>
-        </View>
-      </Overlay>
     </SafeAreaView>
   );
 };
@@ -266,65 +90,26 @@ const styles = StyleSheet.create({
     width: '100%',
   },
   listStyle: {
-    flexDirection: 'row',
-    // justifyContent: 'center',
-    // alignItems: 'center',
-    width: '100%',
-    backgroundColor: 'skyblue',
-  },
-  listItemPartner: {
-    flexGrow: 2,
-    textAlign: 'center',
-    fontSize: 17,
-  },
-  listItemAmount: {
-    flexGrow: 1,
-    textAlign: 'center',
-    fontSize: 17,
+    paddingHorizontal: 15,
+    marginVertical: 5,
   },
   listItemDate: {
-    flexGrow: 2,
-    textAlign: 'center',
+    alignItems: 'flex-end',
     fontSize: 17,
   },
-  listItemPeriod: {
-    flexGrow: 2,
-    textAlign: 'center',
-    fontSize: 17,
+  listItem: {
+    alignItems: 'flex-end',
   },
-  listItemMemo: {
-    flexGrow: 5,
-    textAlign: 'center',
-    fontSize: 17,
+  listItemAmount: {
+    // marginLeft: 10,
+    fontSize: 40,
+    fontWeight: 'bold',
   },
   addButton: {
     position: 'absolute',
     right: 30,
     bottom: 30,
     zIndex: 100,
-  },
-  date: {
-    flexDirection: 'row',
-    // justifyContent: 'flex-start',
-  },
-  formLabel: {
-    position: 'relative',
-    left: 5,
-    top: 3,
-  },
-  dateInput: {
-    width: '100%',
-  },
-  dateButton: {
-    position: 'absolute',
-    right: 22,
-    top: 7,
-  },
-  buttonArea: {
-    flexDirection: 'column',
-    justifyContent: 'center',
-    alignItems: 'center',
-    width: '100%',
   },
   button: {
     marginTop: 10,
@@ -336,5 +121,11 @@ const styles = StyleSheet.create({
     marginLeft: 10,
     position: 'relative',
     top: -15,
+  },
+  icon: {
+    width: 35,
+    height: 35,
+    marginRight: 10,
+    marginTop: 3,
   },
 });
